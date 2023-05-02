fbe355f6ecde38094da618fe0a5b4b96210f078f
MK-IE <45517444+MK-IE@users.noreply.github.com>
2023-05-02 21:17:47 +0100

    [NO-TICKET] Added silly name generator.
    Generates silly names use as part of a bigger program to generate more names (loop etc)

---
+++ silly-name-generator.py
@@ -0,0 +1,22 @@
+from random import choice
+
+first = ('Emma', 'Olivia', 'Ava', 'Isabella', 'Sophia', 'Mia', 'Charlotte', 'Amelia', 'Harper', 'Evelyn',    'Liam', 'Noah', 'William', 'James', 'Oliver', 'Benjamin', 'Elijah', 'Lucas', 'Mason', 'Logan',    'Alexander', 'Ethan', 'Jacob', 'Michael', 'Daniel', 'Henry', 'Jackson', 'Sebastian', 'Aiden', 'Matthew',    'Abigail', 'Emily', 'Sofia', 'Avery', 'Ella', 'Scarlett', 'Grace', 'Chloe', 'Victoria', 'Riley',    'Mila', 'Layla', 'Lily', 'Nora', 'Zoe',
+         'Stella', 'Violet', 'Claire', 'Hazel', 'Aurora',    'Natalie', 'Samantha', 'Maria', 'Lucy', 'Ruby', 'Eva', 'Sophie', 'Sadie', 'Luna', 'Piper',    'Samuel', 'Joseph', 'John', 'David', 'Wyatt', 'Carter', 'Julian', 'Luke', 'Greyson', 'Jayden',    'Owen', 'Gabriel', 'Connor', 'Charles', 'Jaxon', 'Lincoln', 'Christopher', 'Isaiah', 'Andrew', 'Theodore',    'Joshua', 'Nicholas', 'Christian', 'Thomas', 'Aaron', 'Landon', 'Nathan', 'Jonathan', 'Nolan', 'Easton')
+
+last = (
+    'Fintacor', 'Zelthorn', 'Brinmara', 'Quilthorpe', 'Mendaville', 'Glarinford', 'Vendalight', 'Strinweaver',
+    'Crevaston', 'Tinthallow', 'Ferrowist', 'Chromagear', 'Drethcage', 'Vaneshadow', 'Flarowist', 'Strynewood',
+    'Thistledark', 'Velvetmire', 'Whispfrost', 'Grimestone', 'Cobaltglen', 'Duskwhistle', 'Valthorne', 'Mistcreek',
+    'Rivenbrook', 'Stormgarde', 'Glimmerveil', 'Nethermire', 'Shadewhisper', 'Frostgrove', 'Silentfen', 'Gloomcrag',
+    'Galehollow', 'Emberstone', 'Cinderwood', 'Wraithcove', 'Phantomhollow', 'Shadowbriar', 'Ironhollow',
+    'Goblinwatch', 'Tidewhisper', 'Ghosthollow', 'Ravencrest', 'Ebonmire', 'Doomspire', 'Balewind', 'Duskweaver',
+    'Grimward', 'Nightsorrow', 'Thundermire', 'Dreadscar', 'Eclipsemourn', 'Umbralcross', 'Duskcloak', 'Woebringer',
+    'Banespire', 'Darktide', 'Gloombane', 'Shadowthorn', 'Voidmourn', 'Cryptshadow', 'Sorrowfen', 'Gloomspire',
+    'Rimehollow', 'Blightglen', 'Bramblefrost', 'Sablethorn', 'Moonshadow', 'Gravewhisper', 'Sorrowveil',
+    'Abyssfen', 'Hauntgrove', 'Blackcrag', 'Nightscar', 'Chillgarde', 'Bleakmourn', 'Duskcrag', 'Griefhollow',
+    'Deathgrove', 'Hollowcross', 'Wraithgarde', 'Doomwhisper', 'Gloomhollow', 'Felshroud', 'Ruinwatch',
+    'Shadowgarde', 'Nethercrag', 'Doomhollow', 'Gravegarde', 'Hauntspire', 'Ebonfen', 'Balegrove', 'Shadowmire',
+    'Umbragarde', 'Wraithspire', 'Frostscar', 'Nightwhisper'
+)
+
+print(f'{choice(first)} {choice(last)}')
